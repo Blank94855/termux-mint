b58287c85a4690168469ebd18a0c638d1c23feb8
Blank9485 <blank9485.net@gmail.com>
2025-05-24 18:58:01 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,10 @@ clear
 
 Color_Off='\033[0m'
 Red='\033[0;31m'
-Green='\033[0;32m'
+Green='\033[1;32m'
+Blue='\033[1;34m'
+Yellow='\033[1;33m'
+Purple='\033[0;35m'
 Cyan='\033[0;36m'
 
 config="$PREFIX/etc/fish/config.fish"
@@ -11,12 +14,12 @@ config="$PREFIX/etc/fish/config.fish"
 clear
 
 prerequisite() {
-	{ echo; echo -e "${Green}Installing Dependencies..."${CYAN}; echo; }
-	if [[ (-f $PREFIX/bin/fish) && (-f $PREFIX/bin/figlet) && (-f $PREFIX/bin/neofetch)]]; then
-		{ echo "${Green}Dependencies are already Installed!"; }
+	{ echo; echo -e "${Yellow}checking dependencies..."${Cyan}; echo; }
+	if [[ (-f $PREFIX/bin/fish) && (-f $PREFIX/bin/figlet) && (-f $PREFIX/bin/neofetch) ]]; then
+		{ echo "${Green}all dependencies already installed."; }
 	else
 		{ pkg update -y; pkg install -y fish figlet neofetch -y; }
-		(type -p fish figlet neofetch &> /dev/null) && { echo; echo "${Green}Dependencies are succesfully installed!"; } || { echo; echo "${Red}Error Occured, failed to install dependencies."; echo -e $Color_Off;  exit 1; }
+		(type -p fish figlet neofetch &> /dev/null) && { echo; echo "${Green}dependencies installed."; } || { echo; echo "${Red}failed to install deps."; echo -e $Color_Off;  exit 1; }
 	fi
 }
 
@@ -26,49 +29,45 @@ set -U fish_greeting
 clear
 
 echo -e "function __fish_command_not_found_handler --on-event fish_command_not_found
-	/data/data/com.termux/files/usr/libexec/termux/command-not-found $argv[1]
+	/data/data/com.termux/files/usr/libexec/termux/command-not-found \$argv[1]
 end
 function cls
     clear
 end
 " > "$config"
 
-echo -e $Red
-figlet -f smslant "Termux Fish"
+echo -e $Purple
+figlet -f smslant "MintOS Shell"
 echo -e $Color_Off
 printf '\n'
-echo -e "${Green}[*]Clearing greeting text of termux..."
+echo -e "${Blue}[*]removing termux greeting..."
 sleep 2s
-[[( -f "$PREFIX/etc/motd")]] && rm "$PREFIX/etc/motd"
+[[ -f "$PREFIX/etc/motd" ]] && rm "$PREFIX/etc/motd"
 printf '\n'
-echo -e $Cyan"*Cleared greeting text of termux*"
+echo -e $Cyan"*removed greeting text*"
 printf '\n'
-if [[ (-f $PREFIX/etc/motd) ]]
-then
-	rm $PREFIX/etc/motd
-fi
 sleep 2s
-echo -e $Green"[*]Adding neofetch to homepage..." $Red
+echo -e $Green"[*]adding neofetch to mintos..." $Red
 printf '\n'
 sleep 2s
 while true; do
-    read -p "Do you want the android logo in homepage?(y/n)" yn
+    read -p "show android logo on startup? (y/n): " yn
     case $yn in
         [Yy]* ) echo "neofetch" >> "$config"; break;;
         [Nn]* ) echo "neofetch --off" >> "$config"; break;;
-        * ) echo "Please answer yes(y) or no(n).";;
+        * ) echo "enter y or n";;
     esac
 done
 printf '\n'
 sleep 2s
-echo -e $Cyan"*Added neofetch to homepage*" $Green
+echo -e $Cyan"*neofetch added*" $Green
 printf '\n'
 sleep 2s
-echo -e "[*]Setting fish as the default shell..." $Cyan
+echo -e "[*]setting mintos as default shell..." $Blue
 printf '\n'
 sleep 2s
 chsh -s fish
-echo -e "*Set fish as the default shell*"
+echo -e "*mintos set as default shell*"
 sleep 2s
 printf '\n'
-printf  $Green"Done!\n\nNow restart termux.\n\n"
+printf  $Yellow"done.\n\nrestart termux.\n\n"
